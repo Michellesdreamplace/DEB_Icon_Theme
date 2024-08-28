2a5446ce862ef43c7eb344780843980dd0ca28cd
Michelle <172150370+Michellesdreamplace@users.noreply.github.com>
2024-08-28 16:06:58 +0200

    update

--- install_icon_theme.sh
+++ install_icon_theme.sh
@@ -20,12 +20,12 @@ echo "    installiere tar ..."
 echo " ..."
 sudo apt install tar -y
 echo "************************************************************"
+echo "    erstelle ".icons" Verzeichnis, falls nicht vorhanden ..."
+mkdir -p ~/.icons
+echo "************************************************************"
 echo "    klone Icon-Themes von GitHub ..."
 echo " ..."
 sudo git clone https://github.com/Michellesdreamplace/DEB_Icon_Theme.git ~/Downloads/MDP_DEB_Icon_Theme/
-echo "************************************************************"
-echo "    erstelle ".icons" Verzeichnis, falls nicht vorhanden ..."
-sudo mkdir -p ~/.icons
 echo "************************************************************"
 echo "    entpacke Icon-Themes in globalen Freigabeordner ..."
 echo " ..."
